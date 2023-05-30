8469315f8c7e873f17d3cc914feb98e22385da1b
Michal Budzowski <michal.budzowski@sonarsource.com>
2023-05-30 16:49:38 +0200

    Add project files.

---
+++ ConsoleApp65/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp65
+{
+    internal class Class1 : Interface1
+    {
+        private int func(int i)
+        {
+            return i++;
+        }
+    }
+}
